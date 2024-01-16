77b4f3ea97b7876afe6fab29e48f1a0e80d15750
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-01-16 16:03:33 +0000

    Update azureLamp.sh

--- azureLamp.sh
+++ azureLamp.sh
@@ -30,13 +30,14 @@ az vm open-port \
   --port 80,443,3389 &&
 ssh -t -oStrictHostKeyChecking=no azureuser@$(az vm show -d -g myResourceGroup -n myVM --query publicIps -o tsv)      \
 '\
-echo Installing LAMP &&
+echo Installing LAMP... &&
 sudo apt update -qq && sudo apt install -qq -f apache2 mysql-server php &&
+echo Configuring LAMP... &&
 sudo sed -i.bak -e "s/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g" /etc/apache2/mods-enabled/dir.conf &&
 sudo touch /var/www/html/info.php;sudo chmod 666 /var/www/html/info.php;sudo echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 
-echo Installing Adminer &&
-sudo apt -y install adminer && 
+echo Installing Adminer... &&
+sudo apt -qq install adminer && 
 sudo a2enconf adminer && 
 sudo systemctl reload apache2 &&
 sudo mysql -Bse "CREATE USER IF NOT EXISTS admin@localhost IDENTIFIED BY \"password\";GRANT ALL PRIVILEGES ON *.* TO admin@localhost;FLUSH PRIVILEGES;"
@@ -47,17 +48,18 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean 
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password 'password'" # Set MySQL application password for phpmyadmin &&
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password 'password'" # Confirm application password &&
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/internal/skip-preseed boolean true" &&
-DEBIAN_FRONTEND=noninteractive sudo apt -qy install phpmyadmin &&
+DEBIAN_FRONTEND=noninteractive sudo apt -qq install phpmyadmin &&
 
-echo Enable root login for SFTP
+echo Enable root login for SFTP...
 sudo sed -i "/PermitRootLogin/c\PermitRootLogin yes" /etc/ssh/sshd_config &&
 sudo echo -e "login2VM1234\nlogin2VM1234" | sudo passwd root &&
 sudo service sshd restart
+
 printf "\nOpen an internet browser (e.g. Chrome) and go to \e[3;4;33mhttp://$(dig +short myip.opendns.com @resolver1.opendns.com)\e[0m - You should see the Apache default page.\n"
 printf "\nOpen an internet browser (e.g. Chrome) and go to \e[3;4;33mhttp://$(dig +short myip.opendns.com @resolver1.opendns.com)/info.php\e[0m - You should see a PHP info page.\n"
 printf "\nOpen an internet browser (e.g. Chrome) and go to \e[3;4;33mhttp://$(dig +short myip.opendns.com @resolver1.opendns.com)/adminer/?username=admin\e[0m - You should see the Adminer Login page. Username is admin and password is password. Leave Database empty.\n"
 printf "\nOpen an internet browser (e.g. Chrome) and go to \e[3;4;33mhttp://$(dig +short myip.opendns.com @resolver1.opendns.com)/phpmyadmin\e[0m - You should see the phpMyAdmin login page. admin/password\n"
-printf "\nOpen an internet browser (e.g. Edge) and go to \e[3;4;33mhttps://tinyurl.com/47k4bwcr\e[0m - This will download WinSCP. Connect yo your VM with Hostname/ User/ Password = $(dig +short myip.opendns.com @resolver1.opendns.com) / root / login2VM1234.\n"
+printf "\nOpen an internet browser (e.g. Edge) and go to \e[3;4;33mhttps://tinyurl.com/47k4bwcr\e[0m - This will download WinSCP. Connect to your VM --> Hostname = $(dig +short myip.opendns.com @resolver1.opendns.com), User = root, Password = login2VM1234.\n"
 echo Staying logged into this new VM
 bash -l
 '
