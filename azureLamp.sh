08874db81eeba224ffdbe394c57f2c00ed9b1abe
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-02-06 19:27:05 +0000

    Update azureLamp.sh

--- azureLamp.sh
+++ azureLamp.sh
@@ -70,3 +70,12 @@ ssh -t -oStrictHostKeyChecking=no azureuser@$(az vm show -d -g LAMPResourceGroup
 #echo Staying logged into this new VM
 #bash -l
 #'
+
+# Increase you VM size to 
+#sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+#sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+#sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+#sudo apt update
+#sudo apt install code
+#code --install-extension ms-vscode.remote-server
+#sudo code tunnel
